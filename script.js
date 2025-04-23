162aed88746794e5af2ef89d694044c35341bf29
1tash1 <yassinesouf578@gmail.com>
2025-04-23 21:28:34 +0100

    Your message

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function verifyForm(){
+  username=document.getElementById("username").value
+  email=document.getElementById("email").value
+  pass=document.getElementById("password").value
+  confirmpass=document.getElementById("confirmPassword").value
+
+  if(username.length<4){
+    alert("username must be longer than 4 carctere");
+    return false
+  }
+  if(pass.length<6){
+    alert("password must be longer than 6 caractere")
+    return false
+  }
+  if(pass!=confirmpass){
+    alert("the confirm password doesn't match with the first password ")
+    return false
+  }
+} 
